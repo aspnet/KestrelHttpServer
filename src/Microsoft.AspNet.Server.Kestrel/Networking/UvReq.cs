3aaafdff5b56266f4c26cfd765b9a681d32cdb79
Kai Ruhnau <kai.ruhnau@target-sg.com>
2015-02-08 12:09:18 +0100

    Moved a class to a separate file. Fixed a file name

---
+++ src/Microsoft.AspNet.Server.Kestrel/Networking/UvReq.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Microsoft.AspNet.Server.Kestrel.Networking
+{
+    public abstract class UvReq : UvMemory
+    {
+        public UvReq(int threadId, int size)
+            : base(threadId, size)
+        { }
+
+        protected override bool ReleaseHandle()
+        {
+            DestroyMemory(handle);
+            handle = IntPtr.Zero;
+            return true;
+        }
+    }
+}
--- src/Microsoft.AspNet.Server.Kestrel/Networking/UvWriteRequest.cs
+++ src/Microsoft.AspNet.Server.Kestrel/Networking/UvWriteRequest.cs
@@ -92,18 +92,4 @@ namespace Microsoft.AspNet.Server.Kestrel.Networking
             }
         }
     }
-
-    public abstract class UvReq : UvMemory
-    {
-        public UvReq(int threadId, int size)
-            : base(threadId, size)
-        { }
-
-        protected override bool ReleaseHandle()
-        {
-            DestroyMemory(handle);
-            handle = IntPtr.Zero;
-            return true;
-        }
-    }
 }
