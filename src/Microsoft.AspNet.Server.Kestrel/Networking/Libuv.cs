0a677ef9c9d311c87d0d191a9ad2b5de4b18bcd4
Kai Ruhnau <kai.ruhnau@target-sg.com>
2015-02-09 20:13:05 +0100

    Restored the manual native library loading and method binding

--- src/Microsoft.AspNet.Server.Kestrel/Networking/Libuv.cs
+++ src/Microsoft.AspNet.Server.Kestrel/Networking/Libuv.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Microsoft.AspNet.Server.Kestrel.Networking
 {
@@ -18,6 +19,34 @@ namespace Microsoft.AspNet.Server.Kestrel.Networking
                 var p = (int)Environment.OSVersion.Platform;
                 return (p != 4) && (p != 6) && (p != 128);
 #endif
+            }
+        }
+
+        [DllImport("libc", CharSet = CharSet.Ansi)]
+        static extern int uname([Out] StringBuilder buf);
+
+        public static bool IsDarwin
+        {
+            get
+            {
+                // According to the documentation,
+                //  there might be 9, 33, 65, or 257 bytes
+                var buffer = new StringBuilder(8192);
+                try
+                {
+                    if (uname(buffer) == 0)
+                    {
+                        return string.Equals(
+                            buffer.ToString(),
+                            "Darwin",
+                            StringComparison.Ordinal);
+                    }
+                }
+                catch (Exception)
+                {
+                }
+
+                return false;
             }
         }
 
