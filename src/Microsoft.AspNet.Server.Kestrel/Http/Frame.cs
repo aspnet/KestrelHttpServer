7f50736f97774ee8ef4987df37c4fec51d28c3e3
Ben Adams <thundercat@illyriad.co.uk>
2015-11-01 07:16:22 +0000

    Reduce GetString allocs and conversions

--- src/Microsoft.AspNet.Server.Kestrel/Http/Frame.cs
+++ src/Microsoft.AspNet.Server.Kestrel/Http/Frame.cs
@@ -629,7 +629,7 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                 {
                     return false;
                 }
-                var method = begin.GetString(scan);
+                var method = begin.GetAsciiString(ref scan);
 
                 scan.Take();
                 begin = scan;
@@ -653,7 +653,7 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                     {
                         return false;
                     }
-                    queryString = begin.GetString(scan);
+                    queryString = begin.GetAsciiString(ref scan);
                 }
 
                 scan.Take();
@@ -662,7 +662,7 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                 {
                     return false;
                 }
-                var httpVersion = begin.GetString(scan);
+                var httpVersion = begin.GetAsciiString(ref scan);
 
                 scan.Take();
                 if (scan.Take() != '\n')
@@ -670,12 +670,16 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                     return false;
                 }
 
+                string requestUrlPath;
                 if (needDecode)
                 {
                     pathEnd = UrlPathDecoder.Unescape(pathBegin, pathEnd);
+                    requestUrlPath = pathBegin.GetUtf8String(ref pathEnd);
                 }
-
-                var requestUrlPath = pathBegin.GetString(pathEnd);
+                else
+                {
+                    requestUrlPath = pathBegin.GetAsciiString(ref pathEnd);
+                }
 
                 consumed = scan;
                 Method = method;
@@ -689,11 +693,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             {
                 input.ConsumingComplete(consumed, scan);
             }
-        }
-
-        static string GetString(ArraySegment<byte> range, int startIndex, int endIndex)
-        {
-            return Encoding.UTF8.GetString(range.Array, range.Offset + startIndex, endIndex - startIndex);
         }
 
         public static bool TakeMessageHeaders(SocketInput input, FrameRequestHeaders requestHeaders)
@@ -786,8 +785,8 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                             continue;
                         }
 
-                        var name = beginName.GetArraySegment(endName);
-                        var value = beginValue.GetString(endValue);
+                        var name = beginName.GetArraySegment(ref endName);
+                        var value = beginValue.GetAsciiString(ref endValue);
                         if (wrapping)
                         {
                             value = value.Replace("\r\n", " ");
