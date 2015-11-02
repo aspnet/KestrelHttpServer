ac232af4a9089c0bf3569d4267a1898df4c3e2da
Ben Adams <thundercat@illyriad.co.uk>
2015-11-02 01:50:17 +0000

    Explain why dispose can't be used

--- src/Microsoft.AspNet.Server.Kestrel/Http/FrameResponseStream.cs
+++ src/Microsoft.AspNet.Server.Kestrel/Http/FrameResponseStream.cs
@@ -90,6 +90,8 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
         }
         public void StopAcceptingWrites()
         {
+            // Can't use dispose (or close) as can be disposed too early by user code
+            // As exampled in EngineTests.ZeroContentLengthNotSetAutomaticallyForCertainStatusCodes
             _stopped = true;
         }
     }
