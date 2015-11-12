9e62d65787f6d9c69e5aac50b8d4356028fd9119
Master T <tom.deseyn@gmail.com>
2015-11-12 23:53:59 +0100

    Add ClientCertificateValidationCallback option, add test to see the certificate is passed to the HttpContext

--- test/Microsoft.AspNet.Server.KestrelTests/HttpsConnectionFilterTests.cs
+++ test/Microsoft.AspNet.Server.KestrelTests/HttpsConnectionFilterTests.cs
@@ -4,10 +4,13 @@
 #if DNX451
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Security;
+using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Http.Features;
@@ -158,6 +161,70 @@ namespace Microsoft.AspNet.Server.KestrelTests
                         var result = await client.GetAsync(serverAddress);
 
                         Assert.Equal("hello world", await result.Content.ReadAsStringAsync());
+                    }
+                }
+            }
+            finally
+            {
+                ServicePointManager.ServerCertificateValidationCallback -= validationCallback;
+            }
+        }
+
+        // https://github.com/aspnet/KestrelHttpServer/issues/240
+        // This test currently fails on mono because of an issue with SslStream.
+        [ConditionalFact]
+        [OSSkipCondition(OperatingSystems.Linux)]
+        [OSSkipCondition(OperatingSystems.MacOSX)]
+        public async Task CertificatePassedToHttpContext()
+        {
+            RemoteCertificateValidationCallback validationCallback =
+                    (sender, cert, chain, sslPolicyErrors) => true;
+
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback += validationCallback;
+
+                var serverAddress = "https://localhost:54321/";
+                var serviceContext = new TestServiceContext()
+                {
+                    ConnectionFilter = new HttpsConnectionFilter(
+                        new HttpsConnectionFilterOptions
+                        {
+                            ServerCertificate = new X509Certificate2(@"TestResources/testCert.pfx", "testPassword"),
+                            ClientCertificateMode = ClientCertificateMode.RequireCertificate,
+                            ClientCertificateValidation = (certificate, chain, sslPolicyErrors) => true
+                        },
+                        new NoOpConnectionFilter())
+                };
+
+                RequestDelegate app = context =>
+                {
+                    var tlsFeature = context.Features.Get<ITlsConnectionFeature>();
+                    Assert.NotNull(tlsFeature);
+                    Assert.NotNull(tlsFeature.ClientCertificate);
+                    Assert.NotNull(context.Connection.ClientCertificate);
+                    return context.Response.WriteAsync("hello world");
+                };
+
+                using (var server = new TestServer(app, serviceContext, serverAddress))
+                {
+                    // SslStream is used to ensure the certificate is actually passed to the server
+                    // HttpClient might not send the certificate because it is invalid or it doesn't match any
+                    // of the certificate authorities sent by the server in the SSL handshake.
+                    using (var client = new TcpClient())
+                    {
+                        await client.ConnectAsync("127.0.0.1", 54321);
+
+                        SslStream stream = new SslStream(client.GetStream(), false, (sender, certificate, chain, errors) => true,
+                            (sender, host, certificates, certificate, issuers) => new X509Certificate2(@"TestResources/testCert.pfx", "testPassword"));
+                        await stream.AuthenticateAsClientAsync("localhost");
+
+                        var request = Encoding.UTF8.GetBytes("GET / HTTP/1.0\r\n\r\n");
+                        await stream.WriteAsync(request, 0, request.Length);
+
+                        var reader = new StreamReader(stream);
+                        var line = await reader.ReadLineAsync();
+                        Assert.Equal("HTTP/1.0 200 OK", line);
                     }
                 }
             }
