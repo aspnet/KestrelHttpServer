c61160b78124b9ceae912cc584d4a6f0eec0d72b
John Luo <jt.luo@mail.utoronto.ca>
2015-12-16 12:34:36 -0800

    Reacting to new Hosting API

--- samples/LargeResponseApp/Startup.cs
+++ samples/LargeResponseApp/Startup.cs
@@ -1,9 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNet.Builder;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 
 namespace LargeResponseApp
 {
@@ -33,6 +34,18 @@ namespace LargeResponseApp
                     await context.Response.Body.WriteAsync(_chunk, 0, _chunkSize).ConfigureAwait(false);
                 }
             });
+        }
+
+        public static void Main(string[] args)
+        {
+            var config = WebApplicationConfiguration.GetDefault(args);
+
+            var application = new WebApplicationBuilder()
+                .UseConfiguration(config)
+                .UseStartup<Startup>()
+                .Build();
+
+            application.Run();
         }
     }
 }
