f0498713d3e5f704491958603fa126979c5cbeaf
Ben Adams <thundercat@illyriad.co.uk>
2015-12-23 10:15:02 +0000

    Fast path completed Input reads

--- src/Microsoft.AspNet.Server.Kestrel/Http/SocketInputExtensions.cs
+++ src/Microsoft.AspNet.Server.Kestrel/Http/SocketInputExtensions.cs
@@ -1,14 +1,36 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Server.Kestrel.Infrastructure;
 
 namespace Microsoft.AspNet.Server.Kestrel.Http
 {
     public static class SocketInputExtensions
     {
-        public static async Task<int> ReadAsync(this SocketInput input, byte[] buffer, int offset, int count)
+        public static Task<int> ReadAsync(this SocketInput input, byte[] buffer, int offset, int count)
+        {
+            while (input.IsCompleted)
+            {
+                var begin = input.ConsumingStart();
+                int actual;
+                var end = begin.CopyTo(buffer, offset, count, out actual);
+                input.ConsumingComplete(end, end);
+
+                if (actual != 0)
+                {
+                    return Task.FromResult(actual);
+                }
+                if (input.RemoteIntakeFin)
+                {
+                    return TaskUtilities.ZeroTask;
+                }
+            }
+
+            return input.ReadAsyncAwaited(buffer, offset, count);
+        }
+
+        private static async Task<int> ReadAsyncAwaited(this SocketInput input, byte[] buffer, int offset, int count)
         {
             while (true)
             {
