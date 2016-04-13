9fb31b820a989e692b2a1d26a7703f294514da9f
Mike Harder <mharder@microsoft.com>
2016-04-13 11:59:59 -0700

    Remove using Microsoft.AspNetCore.Server.Kestrel.Filter.  It's no longer needed since the UseConnectionLogging() extension method was moved to Microsoft.AspNetCore.Server.Kestrel.

--- samples/SampleApp/Startup.cs
+++ samples/SampleApp/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.Kestrel;
-using Microsoft.AspNetCore.Server.Kestrel.Filter;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.PlatformAbstractions;
 
