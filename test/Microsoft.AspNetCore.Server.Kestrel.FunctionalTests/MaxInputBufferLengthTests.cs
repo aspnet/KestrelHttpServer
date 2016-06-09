45445d4291353b87f8aa6a8edd1a1d9c342fb4ee
Mike Harder <mharder@microsoft.com>
2016-06-09 15:00:08 -0700

    Add test for MaxInputBufferSize=1. Add comments explaining why each size is tested.

--- test/Microsoft.AspNetCore.Server.Kestrel.FunctionalTests/MaxInputBufferLengthTests.cs
+++ test/Microsoft.AspNetCore.Server.Kestrel.FunctionalTests/MaxInputBufferLengthTests.cs
@@ -25,16 +25,31 @@ namespace Microsoft.AspNetCore.Server.Kestrel.FunctionalTests
             get
             {
                 var maxInputBufferLengthValues = new Tuple<int?, bool>[] {
+                    // Smallest allowed buffer.  Server should call pause/resume between each read.
+                    Tuple.Create((int?)1, true),                    
+                    
+                    // Small buffer, but large enough to hold all request headers.
                     Tuple.Create((int?)16 * 1024, true),
+
+                    // Default buffer.
                     Tuple.Create((int?)1024 * 1024, true),
+
+                    // Larger than default, but still 5MB lower than data, so client should be paused.
+                    // On Windows, the client is usually paused at (MaxInputBufferSize + 700,000).
                     Tuple.Create((int?)5 * 1024 * 1024, true),
                     
                     // Even though maxInputBufferLength < _dataLength, client should not be paused since the
-                    // OS-level buffers in client and/or server will handle the overflow
+                    // OS-level buffers in client and/or server will handle the overflow.
                     Tuple.Create((int?)10 * 1024 * 1024 - 1, false),
 
+                    // Buffer is exactly the same size as data.  Exposed race condition where
+                    // IConnectionControl.Resume() was called after socket was disconnected.
                     Tuple.Create((int?)10 * 1024 * 1024, false),
+
+                    // Largest possible buffer, should never trigger backpressure.
                     Tuple.Create((int?)Int32.MaxValue, false),
+
+                    // Disables all code related to computing and limiting the size of the input buffer.
                     Tuple.Create((int?)null, false)
                 };
                 var sendContentLengthHeaderValues = new[] { true, false };
