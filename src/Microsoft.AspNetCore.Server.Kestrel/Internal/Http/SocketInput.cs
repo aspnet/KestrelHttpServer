aspnet/KestrelHttpServer

commit 584f31424c321402da14d63c627207af763b7d19

Only compute consumed length if _bufferLengthControl is not null.

src/Microsoft.AspNetCore.Server.Kestrel/Internal/Http/SocketInput.cs
@@ -197,7 +197,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Internal.Http
                 {
                     if (!consumed.IsDefault)
                     {
-                        var lengthConsumed = new MemoryPoolIterator(_head).GetLength(consumed);
 
                         returnStart = _head;
                         returnEnd = consumed.Block;
@@ -206,7 +205,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Internal.Http
 
                         // Must call Subtract() after _head has been advanced, to avoid producer starting too early and growing
                         // buffer beyond max length.
-                        _bufferLengthControl?.Subtract(lengthConsumed);
+                        _bufferLengthControl?.Subtract(new MemoryPoolIterator(returnStart).GetLength(consumed));
                     }
 
                     if (!examined.IsDefault &&

commit 8047701e924482fae32bcaa8997711efd4ee6835

Compute lengthConsumed before modifying _head or consumed - Reverts regression introduced in 584f314

src/Microsoft.AspNetCore.Server.Kestrel/Internal/Http/SocketInput.cs
@@ -197,6 +197,12 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Internal.Http
                 {
                     if (!consumed.IsDefault)
                     {
+                        // Compute lengthConsumed before modifying _head or consumed
+                        var lengthConsumed = 0;
+                        if (_bufferLengthControl != null)
+                        {
+                            lengthConsumed = new MemoryPoolIterator(_head).GetLength(consumed);
+                        }
 
                         returnStart = _head;
                         returnEnd = consumed.Block;
@@ -205,7 +211,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Internal.Http
 
                         // Must call Subtract() after _head has been advanced, to avoid producer starting too early and growing
                         // buffer beyond max length.
-                        _bufferLengthControl?.Subtract(new MemoryPoolIterator(returnStart).GetLength(consumed));
+                        _bufferLengthControl?.Subtract(lengthConsumed);
                     }
 
                     if (!examined.IsDefault &&