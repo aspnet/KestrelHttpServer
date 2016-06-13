1469b42e717f379ec363d80d15a4568d6a5a9b5e
Mike Harder <mharder@microsoft.com>
2016-06-13 14:14:20 -0700

    Further improve doc comment for MaxInputBufferLength.

--- src/Microsoft.AspNetCore.Server.Kestrel/KestrelServerOptions.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel/KestrelServerOptions.cs
@@ -23,7 +23,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel
 
         /// <summary>
         /// Maximum number of bytes used to buffer input for each connection.
-        /// Default is 1,048,576 bytes (1 MB).  If value is null, the number of bytes is unlimited.
+        /// Default is 1,048,576 bytes (1 MB).  If value is null, the length of the input buffer is unlimited.
         /// </summary>
         public int? MaxInputBufferLength
         {
