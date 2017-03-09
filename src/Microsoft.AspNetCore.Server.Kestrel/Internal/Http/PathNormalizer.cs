16607b0932a1348dfe2972aae6a4582c4dae0e9c
Cesar Blum Silveira <cesars@microsoft.com>
2017-03-09 11:34:22 -0800

    Improve compliance to spec algorithm; add comments.

--- src/Microsoft.AspNetCore.Server.Kestrel/Internal/Http/PathNormalizer.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel/Internal/Http/PathNormalizer.cs
@@ -8,6 +8,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Internal.Http
 {
     public static class PathNormalizer
     {
+        // In-place implementation of the algorithm from https://tools.ietf.org/html/rfc3986#section-5.2.4
         public static unsafe int RemoveDotSegments(Span<byte> span)
         {
             var length = span.Length;
@@ -22,24 +23,57 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Internal.Http
                 {
                     if (end - src >= 3 && *src == '.' && *(src + 1) == '.' && *(src + 2) == '/')
                     {
+                        // A.  If the input buffer begins with a prefix of "../" or "./",
+                        //     then remove that prefix from the input buffer; otherwise,
                         src += 3;
                     }
                     else if (end - src >= 2 && *src == '.' && *(src + 1) == '/')
                     {
+                        // A.  If the input buffer begins with a prefix of "../" or "./",
+                        //     then remove that prefix from the input buffer; otherwise,
                         src += 2;
                     }
                     else if (end - src >= 3 && *src == '/' && *(src + 1) == '.' && *(src + 2) == '/')
                     {
+                        // B.  if the input buffer begins with a prefix of "/./" or "/.",
+                        //     where "." is a complete path segment, then replace that
+                        //     prefix with "/" in the input buffer; otherwise,
                         src += 2;
                     }
                     else if (end - src == 2 && *src == '/' && *(src + 1) == '.')
                     {
-                        src += 2;
+                        // B.  if the input buffer begins with a prefix of "/./" or "/.",
+                        //     where "." is a complete path segment, then replace that
+                        //     prefix with "/" in the input buffer; otherwise,
+                        src += 1;
+                        *src = (byte)'/';
                     }
-                    else if ((end - src >= 4 && *src == '/' && *(src + 1) == '.' && *(src + 2) == '.' && *(src + 3) == '/') ||
-                             (end - src == 3 && *src == '/' && *(src + 1) == '.' && *(src + 2) == '.'))
+                    else if (end - src >= 4 && *src == '/' && *(src + 1) == '.' && *(src + 2) == '.' && *(src + 3) == '/')
                     {
+                        // C.  if the input buffer begins with a prefix of "/../" or "/..",
+                        //     where ".." is a complete path segment, then replace that
+                        //     prefix with "/" in the input buffer and remove the last
+                        //     segment and its preceding "/" (if any) from the output
+                        //     buffer; otherwise,
                         src += 3;
+
+                        if (dst > start)
+                        {
+                            do
+                            {
+                                dst--;
+                            } while (dst > start && *dst != '/');
+                        }
+                    }
+                    else if (end - src == 3 && *src == '/' && *(src + 1) == '.' && *(src + 2) == '.')
+                    {
+                        // C.  if the input buffer begins with a prefix of "/../" or "/..",
+                        //     where ".." is a complete path segment, then replace that
+                        //     prefix with "/" in the input buffer and remove the last
+                        //     segment and its preceding "/" (if any) from the output
+                        //     buffer; otherwise,
+                        src += 2;
+                        *src = (byte)'/';
 
                         if (dst > start)
                         {
@@ -51,14 +85,22 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Internal.Http
                     }
                     else if (end - src == 1 && *src == '.')
                     {
+                        // D.  if the input buffer consists only of "." or "..", then remove
+                        //     that from the input buffer; otherwise,
                         src += 1;
                     }
                     else if (end - src == 2 && *src == '.' && *(src + 1) == '.')
                     {
+                        // D.  if the input buffer consists only of "." or "..", then remove
+                        //     that from the input buffer; otherwise,
                         src += 2;
                     }
                     else
                     {
+                        // E.  move the first path segment in the input buffer to the end of
+                        //     the output buffer, including the initial "/" character (if
+                        //     any) and any subsequent characters up to, but not including,
+                        //     the next "/" character or the end of the input buffer.
                         do
                         {
                             *dst++ = *src++;
--- test/Microsoft.AspNetCore.Server.KestrelTests/PathNormalizerTests.cs
+++ test/Microsoft.AspNetCore.Server.KestrelTests/PathNormalizerTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.IO.Pipelines;
 using System.Text;
 using Microsoft.AspNetCore.Server.Kestrel.Internal.Http;
 using Xunit;
@@ -23,7 +22,9 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
         [InlineData("/a/b", "/a/./b")]
         [InlineData("/b", "/a/../b")]
         [InlineData("/a/", "/a/./")]
-        [InlineData("/a", "/a/.")]
+        [InlineData("/a/", "/a/.")]
+        [InlineData("/", "/a/../")]
+        [InlineData("/", "/a/..")]
         [InlineData("/", "/a/../b/../")]
         [InlineData("/", "/a/../b/..")]
         [InlineData("/b", "/a/../../b")]
