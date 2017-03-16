13d88a6fe0cc92d207d5280c32e8ac115c375554
Cesar Blum Silveira <cesars@microsoft.com>
2017-03-15 17:00:51 -0700

    Restore unit tests for string-based RemoveDotSegments.

--- test/Microsoft.AspNetCore.Server.KestrelTests/PathNormalizerTests.cs
+++ test/Microsoft.AspNetCore.Server.KestrelTests/PathNormalizerTests.cs
@@ -45,14 +45,6 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
         [InlineData("/a/..b", "/a/..b")]
         [InlineData("/a/b.", "/a/b.")]
         [InlineData("/a/b..", "/a/b..")]
-        //[InlineData("a/b", "a/b")]
-        //[InlineData("a/b/../c", "a/c")]
-        //[InlineData("./a", "a")]
-        //[InlineData("../a", "a")]
-        //[InlineData("./a/b", "a/b")]
-        //[InlineData("../a/../b", "/b")]
-        //[InlineData(".", "/")]
-        //[InlineData("..", "/")]
         [InlineData("/longlong/../short", "/short")]
         [InlineData("/short/../longlong", "/longlong")]
         [InlineData("/longlong/../short/..", "/")]
@@ -60,15 +52,73 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
         [InlineData("/longlong/../short/../", "/")]
         [InlineData("/short/../longlong/../", "/")]
         [InlineData("/", "/")]
-        //[InlineData("*", "*")]
         [InlineData("/no/segments", "/no/segments")]
         [InlineData("/no/segments/", "/no/segments/")]
-        public void RemovesDotSegments(string input, string expected)
+        public void RemovesDotSegmentsSpan(string input, string expected)
         {
             var data = Encoding.ASCII.GetBytes(input);
             var finalLength = PathNormalizer.RemoveDotSegments(new Span<byte>(data));
             Assert.True(finalLength >= 1);
             Assert.Equal(expected, Encoding.ASCII.GetString(data, 0, finalLength));
+        }
+
+        [Theory]
+        [InlineData("/a", "/a")]
+        [InlineData("/a/", "/a/")]
+        [InlineData("/a/b", "/a/b")]
+        [InlineData("/a/b/", "/a/b/")]
+        [InlineData("/a", "/./a")]
+        [InlineData("/a", "/././a")]
+        [InlineData("/a", "/../a")]
+        [InlineData("/a", "/../../a")]
+        [InlineData("/a/b", "/a/./b")]
+        [InlineData("/b", "/a/../b")]
+        [InlineData("/a/", "/a/./")]
+        [InlineData("/a", "/a/.")]
+        [InlineData("/", "/a/../b/../")]
+        [InlineData("/", "/a/../b/..")]
+        [InlineData("/b", "/a/../../b")]
+        [InlineData("/b/", "/a/../../b/")]
+        [InlineData("/b", "/a/.././../b")]
+        [InlineData("/b/", "/a/.././../b/")]
+        [InlineData("/a/d", "/a/b/c/./../../d")]
+        [InlineData("/a/d", "/./a/b/c/./../../d")]
+        [InlineData("/a/d", "/../a/b/c/./../../d")]
+        [InlineData("/a/d", "/./../a/b/c/./../../d")]
+        [InlineData("/a/d", "/.././a/b/c/./../../d")]
+        [InlineData("/.a", "/.a")]
+        [InlineData("/..a", "/..a")]
+        [InlineData("/...", "/...")]
+        [InlineData("/a/.../b", "/a/.../b")]
+        [InlineData("/b", "/a/../.../../b")]
+        [InlineData("/a/.b", "/a/.b")]
+        [InlineData("/a/..b", "/a/..b")]
+        [InlineData("/a/b.", "/a/b.")]
+        [InlineData("/a/b..", "/a/b..")]
+        [InlineData("a/b", "a/b")]
+        [InlineData("a/c", "a/b/../c")]
+        [InlineData("*", "*")]
+        [InlineData("/longlong/../short", "/short")]
+        [InlineData("/short/../longlong", "/longlong")]
+        [InlineData("/longlong/../short/..", "/")]
+        [InlineData("/short/../longlong/..", "/")]
+        [InlineData("/longlong/../short/../", "/")]
+        [InlineData("/short/../longlong/../", "/")]
+        [InlineData("/", "/")]
+        [InlineData("/no/segments", "/no/segments")]
+        [InlineData("/no/segments/", "/no/segments/")]
+        // Not handled by span version
+        [InlineData("./a", "a")]
+        [InlineData("../a", "a")]
+        [InlineData("./a/b", "a/b")]
+        [InlineData("../a/../b", "/b")]
+        [InlineData(".", "/")]
+        [InlineData("..", "/")]
+        [InlineData("*", "*")]
+        public void RemovesDotSegmentsString(string expected, string input)
+        {
+            var result = PathNormalizer.RemoveDotSegments(input);
+            Assert.Equal(expected, result);
         }
     }
 }
