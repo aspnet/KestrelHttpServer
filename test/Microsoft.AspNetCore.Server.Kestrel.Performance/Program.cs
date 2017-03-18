6f1e31ea4756c21195d8cb2b0e150d0aa1c69636
Ben Adams <thundercat@illyriad.co.uk>
2017-03-18 20:57:55 +0000

    Add ResponseHeadersWritingBenchmark (feature-si) (#1505)
    * Add ResponseHeadersWritingBenchmark (feature-si)
    * Use StreamSocketOutput

--- test/Microsoft.AspNetCore.Server.Kestrel.Performance/Program.cs
+++ test/Microsoft.AspNetCore.Server.Kestrel.Performance/Program.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Reflection;
 using BenchmarkDotNet.Running;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Performance
@@ -10,42 +11,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Performance
     {
         public static void Main(string[] args)
         {
-            var options = (uint[])Enum.GetValues(typeof(BenchmarkType));
-            BenchmarkType type;
-            if (args.Length != 1 || !Enum.TryParse(args[0], out type))
-            {
-                Console.WriteLine($"Please add benchmark to run as parameter:");
-                for (var i = 0; i < options.Length; i++)
-                {
-                    Console.WriteLine($"  {((BenchmarkType)options[i]).ToString()}");
-                }
-
-                return;
-            }
-
-            RunSelectedBenchmarks(type);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
         }
-
-        private static void RunSelectedBenchmarks(BenchmarkType type)
-        {
-            if (type.HasFlag(BenchmarkType.RequestParsing))
-            {
-                BenchmarkRunner.Run<RequestParsing>();
-            }
-            if (type.HasFlag(BenchmarkType.Writing))
-            {
-                BenchmarkRunner.Run<Writing>();
-            }
-        }
-    }
-
-    [Flags]
-    public enum BenchmarkType : uint
-    {
-        RequestParsing = 1,
-        Writing = 2,
-        // add new ones in powers of two - e.g. 2,4,8,16...
-
-        All = uint.MaxValue
     }
 }
