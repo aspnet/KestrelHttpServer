b953daa378c925a1237b0ec3f33bb63db1bad001
Stephen Halter <halter73@gmail.com>
2017-03-20 16:57:57 -0700

    Add transport interfaces

---
+++ src/Microsoft.AspNetCore.Server.Kestrel.Core/Transport/IConnectionHandler.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO.Pipelines;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.Transport
+{
+    public interface IConnectionHandler
+    {
+        IConnectionContext OnConnection(IConnectionInformation connectionInfo, PipeOptions inputOptions, PipeOptions outputOptions);
+    }
+}
