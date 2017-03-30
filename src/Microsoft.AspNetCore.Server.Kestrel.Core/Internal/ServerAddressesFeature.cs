b6c69f94652bc38bc6fa952975d6b4dd179e95f8
David Fowler <davidfowl@gmail.com>
2017-03-29 21:54:47 -0700

    wip

---
+++ src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/ServerAddressesFeature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
+{
+    internal class ServerAddressesFeature : IServerAddressesFeature
+    {
+        public ICollection<string> Addresses { get; } = new List<string>();
+    }
+}
