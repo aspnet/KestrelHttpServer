baf9fcb0f0ba6cc38c41663ac935a69ba875ea6c
David Fowler <davidfowl@gmail.com>
2017-03-30 19:43:34 -0700

    Review feedback - Added TlsConnectionFeature to Https

--- src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/ServerAddressesFeature.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/ServerAddressesFeature.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Hosting.Server.Features;
@@ -8,5 +11,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
     internal class ServerAddressesFeature : IServerAddressesFeature
     {
         public ICollection<string> Addresses { get; } = new List<string>();
+        public bool PreferHostingUrls { get; set; }
     }
 }
