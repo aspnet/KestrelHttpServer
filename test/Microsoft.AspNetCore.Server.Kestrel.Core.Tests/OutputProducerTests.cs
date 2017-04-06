1767ff3a88c982ca6973747c46dba677a80812e8
Cesar Blum Silveira <cesars@microsoft.com>
2017-04-06 15:02:58 -0700

    Increase socket receive timeout in MaxRequestBufferSizeTests to mitigate flakiness.

--- test/Microsoft.AspNetCore.Server.Kestrel.Core.Tests/OutputProducerTests.cs
+++ test/Microsoft.AspNetCore.Server.Kestrel.Core.Tests/OutputProducerTests.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.IO.Pipelines;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Internal.System.IO.Pipelines;
 using Microsoft.AspNetCore.Testing;
 using Moq;
 using Xunit;
