c04234983e574dc6f242cb9e9490515699033bd3
Cesar Blum Silveira <cesars@microsoft.com>
2017-04-07 09:52:55 -0700

    Remove unused using.

--- src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/FrameConnection.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/FrameConnection.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure;
 using Microsoft.AspNetCore.Server.Kestrel.Internal.System.IO.Pipelines;
 using Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions;
-using Microsoft.Extensions.Internal;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
