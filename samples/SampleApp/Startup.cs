0e4a37e1a7bd6f297ad257796382e2de37dcd098
Cesar Blum Silveira <cesars@microsoft.com>
2017-04-26 11:11:54 -0700

    Revert changes to SampleApp.

--- samples/SampleApp/Startup.cs
+++ samples/SampleApp/Startup.cs
@@ -19,7 +19,18 @@ namespace SampleApp
         {
             var logger = loggerFactory.CreateLogger("Default");
 
-            app.Run(context => Task.FromResult(0));
+            app.Run(async context =>
+            {
+                var connectionFeature = context.Connection;
+                logger.LogDebug($"Peer: {connectionFeature.RemoteIpAddress?.ToString()}:{connectionFeature.RemotePort}"
+                    + $"{Environment.NewLine}"
+                    + $"Sock: {connectionFeature.LocalIpAddress?.ToString()}:{connectionFeature.LocalPort}");
+
+                var response = $"hello, world{Environment.NewLine}";
+                context.Response.ContentLength = response.Length;
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync(response);
+            });
         }
 
         public static void Main(string[] args)
