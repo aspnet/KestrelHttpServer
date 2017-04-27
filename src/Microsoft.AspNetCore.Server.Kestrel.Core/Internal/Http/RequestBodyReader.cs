e9602f8bcd977113a45b7b43372278294b2dc04c
Cesar Blum Silveira <cesars@microsoft.com>
2017-04-27 10:59:35 -0700

    Move EmptyRequestBodyReader to its own file.

--- src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/Http/RequestBodyReader.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/Http/RequestBodyReader.cs
@@ -157,25 +157,4 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
             }
         }
     }
-
-    public class EmptyRequestBodyReader : IRequestBodyReader
-    {
-        public static readonly IRequestBodyReader Instance = new EmptyRequestBodyReader();
-
-        public Task ConsumeAsync(CancellationToken cancellationToken = default(CancellationToken))
-            => TaskCache.CompletedTask;
-
-        public Task CopyToAsync(Stream destination, CancellationToken cancellationToken = default(CancellationToken))
-            => TaskCache.CompletedTask;
-
-        public Task<int> ReadAsync(ArraySegment<byte> buffer, CancellationToken cancellationToken = default(CancellationToken))
-            => TaskCache<int>.DefaultCompletedTask;
-
-        public void Reset()
-        {
-        }
-
-        public Task StartAsync(MessageBody messageBody, CancellationToken cancellationToken = default(CancellationToken))
-            => TaskCache.CompletedTask;
-    }
 }
