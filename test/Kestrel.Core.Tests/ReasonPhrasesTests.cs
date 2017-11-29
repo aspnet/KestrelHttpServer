c0fac8d0fa8c68f42c4ff6fe8ff1571b29357523
Tornhoof <Tornhoof@gmail.com>
2017-11-29 12:46:39 +0100

    Consolidated test methods and check expectedResult everywhere

--- test/Kestrel.Core.Tests/ReasonPhrasesTests.cs
+++ test/Kestrel.Core.Tests/ReasonPhrasesTests.cs
@@ -12,21 +12,13 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Tests
         [Theory]
         [InlineData(999, "Unknown", "999 Unknown")]
         [InlineData(999, null, "999 ")]
-        public void UnknownStatusCodes(int statusCode, string reasonPhrase, string expectedResult)
+		[InlineData(StatusCodes.Status200OK, "OK", "200 OK")]
+        [InlineData(StatusCodes.Status200OK, null, "200 OK")]
+        public void Formatting(int statusCode, string reasonPhrase, string expectedResult)
         {
             var bytes = Internal.Http.ReasonPhrases.ToStatusBytes(statusCode, reasonPhrase);
             Assert.NotNull(bytes);
             Assert.Equal(expectedResult, Encoding.ASCII.GetString(bytes));
-        }
-
-        [Theory]
-        [InlineData(StatusCodes.Status200OK, "OK")]
-        [InlineData(StatusCodes.Status200OK, null)]
-        public void KnownStatusCodes(int statusCode, string reasonPhrase)
-        {
-            var bytes = Internal.Http.ReasonPhrases.ToStatusBytes(statusCode, reasonPhrase);
-            Assert.NotNull(bytes);
-            Assert.NotEqual(0x20, bytes[bytes.Length - 1]);
         }
     }
 }
