24593f6fbbfe9aee7fb2fe16d40d7c2092ff064c
Marc Gravell <marc.gravell@gmail.com>
2017-12-29 01:30:29 +0000

    extend socket transport factory to allow outbound connections; this is an initial cut only

--- src/Kestrel.Transport.Sockets/SocketTransportFactory.cs
+++ src/Kestrel.Transport.Sockets/SocketTransportFactory.cs
@@ -2,11 +2,16 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Net;
+using System.Buffers;
 using Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal;
 using Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets.Internal;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO.Pipelines;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets
 {
@@ -56,6 +61,53 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets
             }
 
             return new SocketTransport(endPointInformation, handler, _appLifetime, _trace);
+        }
+
+        public static Task<IPipeConnection> ConnectAsync(EndPoint endpoint, MemoryPool memoryPool)
+        {
+            var args = new SocketAsyncEventArgs();
+            args.RemoteEndPoint = endpoint;
+            args.Completed += (sender, p) => OnConnect(p);
+            var tcs = new TaskCompletionSource<IPipeConnection>(memoryPool);
+            args.UserToken = tcs;
+            if (!Socket.ConnectAsync(SocketType.Stream, ProtocolType.Tcp, args))
+            {
+                OnConnect(args); // completed sync - usually means failure
+            }
+            return tcs.Task;
+        }
+
+        private static void OnConnect(SocketAsyncEventArgs e)
+        {
+            var tcs = (TaskCompletionSource<IPipeConnection>)e.UserToken;
+            try
+            {
+                if (e.SocketError == SocketError.Success)
+                {
+                    e.ConnectSocket.NoDelay = true;
+                    var connection = new SocketConnection(e.ConnectSocket, (MemoryPool)tcs.Task.AsyncState, SocketsTrace.Nil);
+
+                    //var options = new PipeOptions(connection.MemoryPool);
+
+                    //var receivePipe = new Pipe(options);
+                    //var sendPipe = new Pipe(options);
+
+                    //var pipe = new PipeConnection(receivePipe.Reader, sendPipe.Writer);
+                    var pair = PipeFactory.CreateConnectionPair(connection.MemoryPool);
+                    connection.Transport = pair.Transport;
+                    connection.Application = pair.Application;
+                    _ = connection.StartAsync(null);
+                    tcs.TrySetResult(connection.Transport);
+                }
+                else
+                {
+                    tcs.TrySetException(new SocketException((int)e.SocketError));
+                }
+            }
+            catch (Exception ex)
+            {
+                tcs.TrySetException(ex);
+            }
         }
     }
 }
