56698a4dd854e4ef5120ed968e5630e629047d11
John Luo <johluo@microsoft.com>
2018-03-26 18:09:40 -0700

    Add more logging

--- test/Kestrel.FunctionalTests/DefaultHeaderTests.cs
+++ test/Kestrel.FunctionalTests/DefaultHeaderTests.cs
@@ -2,46 +2,57 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel.FunctionalTests;
 using Microsoft.AspNetCore.Testing;
+using Microsoft.Extensions.Logging.Testing;
 using Xunit;
+using Xunit.Abstractions;
 
-namespace Microsoft.AspNetCore.Server.Kestrel.FunctionalTests
+namespace FunctionalTests
 {
-    public class DefaultHeaderTests
+    public class DefaultHeaderTests : LoggedTest
     {
+        public DefaultHeaderTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
         [Fact]
         public async Task TestDefaultHeaders()
         {
-            var testContext = new TestServiceContext()
+            using (StartLog(out var loggerFactory, TestConstants.DefaultFunctionalTestLogLevel))
             {
-                ServerOptions = { AddServerHeader = true }
-            };
-
-            using (var server = new TestServer(ctx => Task.CompletedTask, testContext))
-            {
-                using (var connection = server.CreateConnection())
+                var testContext = new TestServiceContext()
                 {
-                    await connection.Send(
-                        "GET / HTTP/1.1",
-                        "Host:",
-                        "",
-                        "GET / HTTP/1.0",
-                        "",
-                        "");
+                    ServerOptions = { AddServerHeader = true },
+                    LoggerFactory = loggerFactory
+                };
 
-                    await connection.ReceiveForcedEnd(
-                        "HTTP/1.1 200 OK",
-                        $"Date: {testContext.DateHeaderValue}",
-                        "Server: Kestrel",
-                        "Content-Length: 0",
-                        "",
-                        "HTTP/1.1 200 OK",
-                        "Connection: close",
-                        $"Date: {testContext.DateHeaderValue}",
-                        "Server: Kestrel",
-                        "Content-Length: 0",
-                        "",
-                        "");
+                using (var server = new TestServer(ctx => Task.CompletedTask, testContext))
+                {
+                    using (var connection = server.CreateConnection())
+                    {
+                        await connection.Send(
+                            "GET / HTTP/1.1",
+                            "Host:",
+                            "",
+                            "GET / HTTP/1.0",
+                            "",
+                            "");
+
+                        await connection.ReceiveForcedEnd(
+                            "HTTP/1.1 200 OK",
+                            $"Date: {testContext.DateHeaderValue}",
+                            "Server: Kestrel",
+                            "Content-Length: 0",
+                            "",
+                            "HTTP/1.1 200 OK",
+                            "Connection: close",
+                            $"Date: {testContext.DateHeaderValue}",
+                            "Server: Kestrel",
+                            "Content-Length: 0",
+                            "",
+                            "");
+                    }
                 }
             }
         }
