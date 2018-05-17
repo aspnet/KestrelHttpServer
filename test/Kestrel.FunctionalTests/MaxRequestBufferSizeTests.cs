4d819c557320d5a33400dcee2937f8fd34774da6
Mike Harder <mharder@microsoft.com>
2018-05-17 15:48:52 -0700

    Add pause data for MaxRequestBufferSize=1MB

--- test/Kestrel.FunctionalTests/MaxRequestBufferSizeTests.cs
+++ test/Kestrel.FunctionalTests/MaxRequestBufferSizeTests.cs
@@ -21,22 +21,27 @@ namespace Microsoft.AspNetCore.Server.Kestrel.FunctionalTests
 {
     public class MaxRequestBufferSizeTests : LoggedTest
     {
-        // When MaxRequestBufferSize=5MB, the client is typically paused after uploading this many bytes:
+        // The client is typically paused after uploading this many bytes:
         // 
-        // OS                   connectionAdapter   Transport   min pause (MB)      max pause (MB)
-        // ---------------      -----------------   ---------   --------------      --------------
-        // Windows 10 1803      false               Libuv       6                   13
-        // Windows 10 1803      false               Sockets     7                   24
-        // Windows 10 1803      true                Libuv       12                  12
-        // Windows 10 1803      true                Sockets     12                  36
-        // Ubuntu 18.04         false               Libuv       13                  15
-        // Ubuntu 18.04         false               Sockets     13                  15
-        // Ubuntu 18.04         true                Libuv       19                  20
-        // Ubuntu 18.04         true                Sockets     18                  20
-        // macOS 10.13.4        false               Libuv       6                   6
-        // macOS 10.13.4        false               Sockets     6                   6
-        // macOS 10.13.4        true                Libuv       11                  11
-        // macOS 10.13.4        true                Sockets     11                  11
+        // OS                   MaxRequestBufferSize (MB)   connectionAdapter   Transport   min pause (MB)      max pause (MB)
+        // ---------------      -------------------------   -----------------   ---------   --------------      --------------
+        // Windows 10 1803      1                           false               Libuv       1.7                 3.3
+        // Windows 10 1803      1                           false               Sockets     1.7                 4.4
+        // Windows 10 1803      1                           true                Libuv       3.0                 8.4
+        // Windows 10 1803      1                           true                Sockets     3.2                 9.0
+        //
+        // Windows 10 1803      5                           false               Libuv       6                   13
+        // Windows 10 1803      5                           false               Sockets     7                   24
+        // Windows 10 1803      5                           true                Libuv       12                  12
+        // Windows 10 1803      5                           true                Sockets     12                  36
+        // Ubuntu 18.04         5                           false               Libuv       13                  15
+        // Ubuntu 18.04         5                           false               Sockets     13                  15
+        // Ubuntu 18.04         5                           true                Libuv       19                  20
+        // Ubuntu 18.04         5                           true                Sockets     18                  20
+        // macOS 10.13.4        5                           false               Libuv       6                   6
+        // macOS 10.13.4        5                           false               Sockets     6                   6
+        // macOS 10.13.4        5                           true                Libuv       11                  11
+        // macOS 10.13.4        5                           true                Sockets     11                  11
         //
         // When connectionAdapter=true, the MaxRequestBufferSize is set on two pipes, so it's effectively doubled.
         //
