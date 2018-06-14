2c44ba934f88abd5c199ac12f4b9b93c86fecedd
Mike Harder <mharder@microsoft.com>
2018-06-14 09:54:01 -0700

    Rename static Socket to match test

--- test/Kestrel.Transport.Libuv.FunctionalTests/ListenHandleTests.cs
+++ test/Kestrel.Transport.Libuv.FunctionalTests/ListenHandleTests.cs
@@ -19,16 +19,18 @@ namespace Microsoft.AspNetCore.Server.Kestrel.FunctionalTests
         // race condition bugs (test hangs) if the file descriptor has been re-used for another resource.  In .NET Core, objects
         // assigned to static fields should never be disposed or finalized (even at process shutdown).
         // https://github.com/aspnet/KestrelHttpServer/issues/2597
-        private static readonly Socket _listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly Socket _canListenToOpenTcpSocketHandleSocket =
+            new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
         [ConditionalFact]
         public async Task CanListenToOpenTcpSocketHandle()
         {
-            _listenSocket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
+            _canListenToOpenTcpSocketHandleSocket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
 
-            using (var server = new TestServer(_ => Task.CompletedTask, new TestServiceContext(LoggerFactory), new ListenOptions((ulong)_listenSocket.Handle)))
+            using (var server = new TestServer(_ => Task.CompletedTask, new TestServiceContext(LoggerFactory),
+                new ListenOptions((ulong)_canListenToOpenTcpSocketHandleSocket.Handle)))
             {
-                using (var connection = new TestConnection(((IPEndPoint)_listenSocket.LocalEndPoint).Port))
+                using (var connection = new TestConnection(((IPEndPoint)_canListenToOpenTcpSocketHandleSocket.LocalEndPoint).Port))
                 {
                     await connection.SendEmptyGet();
 
