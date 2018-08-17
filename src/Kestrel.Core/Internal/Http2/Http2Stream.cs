8a65675d54cff1e916fc63578950f99710008556
David Fowler <davidfowl@gmail.com>
2018-08-17 12:21:47 -0700

    Take 2 at fixing the leak - We didn't wait on the stream loops to complete before the connection died.

--- src/Kestrel.Core/Internal/Http2/Http2Stream.cs
+++ src/Kestrel.Core/Internal/Http2/Http2Stream.cs
@@ -28,6 +28,8 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2
         private StreamCompletionFlags _completionState;
         private readonly object _completionLock = new object();
 
+        private TaskCompletionSource<object> _processTask = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+
         public Http2Stream(Http2StreamContext context)
             : base(context)
         {
@@ -45,6 +47,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2
 
             RequestBodyPipe = CreateRequestBodyPipe();
             Output = _http2Output;
+            ProcessingTask = _processTask.Task;
         }
 
         public int StreamId => _context.StreamId;
@@ -56,6 +59,8 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2
         private bool IsAborted => (_completionState & StreamCompletionFlags.Aborted) == StreamCompletionFlags.Aborted;
 
         public override bool IsUpgradableRequest => false;
+
+        public Task ProcessingTask { get; }
 
         protected override void OnReset()
         {
@@ -73,6 +78,9 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2
             // The app can no longer read any more of the request body, so return any bytes that weren't read to the
             // connection's flow-control window.
             _inputFlowControl.Abort();
+
+            // Mark processing as completed
+            _processTask.TrySetResult(null);
         }
 
         protected override string CreateRequestId()
