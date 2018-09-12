9135cabba2cd92f698e7c340adc750a6d19f462c
Stephen Halter <halter73@gmail.com>
2018-09-11 17:07:31 -0700

    Add in-memory functional test project cherry-pick
    - Run AddressRegistrationTests independently to avoid port conflicts
    - Only run tests that verify transport behavior for each transport
    
    # Conflicts:
    #	src/Kestrel.Transport.Abstractions/Properties/AssemblyInfo.cs
    #	src/Kestrel.Transport.Libuv/AssemblyInfo.cs
    #	src/Kestrel.Transport.Sockets/AssemblyInfo.cs
    #	test/Kestrel.Core.Tests/ConnectionDispatcherTests.cs
    #	test/Kestrel.FunctionalTests/RequestTests.cs
    #	test/Kestrel.InMemory.FunctionalTests/ChunkedRequestTests.cs
    #	test/Kestrel.InMemory.FunctionalTests/Http2/PipeReaderFactory.cs
    #	test/Kestrel.InMemory.FunctionalTests/Http2/TlsTests.cs
    #	test/Kestrel.InMemory.FunctionalTests/HttpsConnectionAdapterTests.cs
    #	test/Kestrel.InMemory.FunctionalTests/RequestBodyTimeoutTests.cs
    #	test/Kestrel.InMemory.FunctionalTests/ResponseTests.cs
    #	test/Kestrel.Transport.FunctionalTests/Http2/H2SpecTests.cs
    #	test/Kestrel.Transport.FunctionalTests/Http2/HandshakeTests.cs
    #	test/Kestrel.Transport.FunctionalTests/Http2/ShutdownTests.cs
    #	test/shared/FunctionalTestHelpers/DiagnosticMemoryPoolFactory.cs
    #	test/shared/TestConnection.cs
    #	test/shared/TransportTestHelpers/H2SpecCommands.cs
    #	test/shared/TransportTestHelpers/TestServer.cs

--- src/Kestrel.Core/KestrelServer.cs
+++ src/Kestrel.Core/KestrelServer.cs
@@ -21,7 +21,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
     public class KestrelServer : IServer
     {
         private readonly List<ITransport> _transports = new List<ITransport>();
-        private readonly Heartbeat _heartbeat;
         private readonly IServerAddressesFeature _serverAddresses;
         private readonly ITransportFactory _transportFactory;
 
@@ -44,13 +43,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
 
             _transportFactory = transportFactory;
             ServiceContext = serviceContext;
-
-            var httpHeartbeatManager = new HttpHeartbeatManager(serviceContext.ConnectionManager);
-            _heartbeat = new Heartbeat(
-                new IHeartbeatHandler[] { serviceContext.DateHeaderValueManager, httpHeartbeatManager },
-                serviceContext.SystemClock,
-                DebuggerWrapper.Singleton,
-                Trace);
 
             Features = new FeatureCollection();
             _serverAddresses = new ServerAddressesFeature();
@@ -78,6 +70,13 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
             var systemClock = new SystemClock();
             var dateHeaderValueManager = new DateHeaderValueManager(systemClock);
 
+            var httpHeartbeatManager = new HttpHeartbeatManager(connectionManager);
+            var heartbeat = new Heartbeat(
+                new IHeartbeatHandler[] { dateHeaderValueManager, httpHeartbeatManager },
+                systemClock,
+                DebuggerWrapper.Singleton,
+                trace);
+
             // TODO: This logic will eventually move into the IConnectionHandler<T> and off
             // the service context once we get to https://github.com/aspnet/KestrelHttpServer/issues/1662
             PipeScheduler scheduler = null;
@@ -102,7 +101,8 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
                 SystemClock = systemClock,
                 DateHeaderValueManager = dateHeaderValueManager,
                 ConnectionManager = connectionManager,
-                ServerOptions = serverOptions
+                Heartbeat = heartbeat,
+                ServerOptions = serverOptions,
             };
         }
 
@@ -133,7 +133,8 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
                     throw new InvalidOperationException(CoreStrings.ServerAlreadyStarted);
                 }
                 _hasStarted = true;
-                _heartbeat.Start();
+
+                ServiceContext.Heartbeat?.Start();
 
                 async Task OnBind(ListenOptions endpoint)
                 {
@@ -199,7 +200,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
                 }
                 await Task.WhenAll(tasks).ConfigureAwait(false);
 
-                _heartbeat.Dispose();
+                ServiceContext.Heartbeat?.Dispose();
             }
             catch (Exception ex)
             {
