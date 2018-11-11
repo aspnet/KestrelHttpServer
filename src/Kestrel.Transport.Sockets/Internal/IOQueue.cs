b7bb5f392df6df3f9bf8d838ba99f99ee127592f
Ben Adams <thundercat@illyriad.co.uk>
2018-11-11 17:26:41 +0000

    Reduce lock contention in IOQueue

--- src/Kestrel.Transport.Sockets/Internal/IOQueue.cs
+++ src/Kestrel.Transport.Sockets/Internal/IOQueue.cs
@@ -8,9 +8,14 @@ using System.Threading;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets.Internal
 {
+#if NETCOREAPP3_0
+    public class IOQueue : PipeScheduler, IThreadPoolWorkItem
+    {
+#else
     public class IOQueue : PipeScheduler
     {
-        private static readonly WaitCallback _doWorkCallback = s => ((IOQueue)s).DoWork();
+        private static readonly WaitCallback _doWorkCallback = s => ((IOQueue)s).Execute();
+#endif
 
         private readonly object _workSync = new object();
         private readonly ConcurrentQueue<Work> _workItems = new ConcurrentQueue<Work>();
@@ -24,19 +29,42 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets.Internal
                 State = state
             };
 
+            // Order is important here with DoWork.
+            // Enqueue prior to checking _doingWork.
             _workItems.Enqueue(work);
 
-            lock (_workSync)
+            if (!Volatile.Read(ref _doingWork))
             {
-                if (!_doingWork)
+                // Not scheduled or working.
+                var submitWork = false;
+                // We re-check under lock here to prevent double schedule or missed schedule.
+                lock (_workSync)
                 {
+                    // Don't schedule if already scheduled or doing work.
+                    if (!_doingWork)
+                    {
+                        submitWork = true;
+                        _doingWork = true;
+                    }
+                }
+
+                // Wasn't scheduled or active, schedule outside lock.
+                if (submitWork)
+                {
+#if NETCOREAPP3_0
+                    System.Threading.ThreadPool.UnsafeQueueUserWorkItem(this, preferLocal: false);
+#else
                     System.Threading.ThreadPool.UnsafeQueueUserWorkItem(_doWorkCallback, this);
-                    _doingWork = true;
+#endif
                 }
             }
         }
 
-        private void DoWork()
+#if NETCOREAPP3_0
+        void IThreadPoolWorkItem.Execute()
+#else
+        private void Execute()
+#endif
         {
             while (true)
             {
@@ -45,12 +73,29 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets.Internal
                     item.Callback(item.State);
                 }
 
+                // Order is important here with Schedule.
+                // Set _doingWork prior to checking .IsEmpty
+                Volatile.Write(ref _doingWork, false);
+
+                // We check under lock here to prevent double schedule or missed schedule.
                 lock (_workSync)
                 {
                     if (_workItems.IsEmpty)
                     {
-                        _doingWork = false;
-                        return;
+                        // Nothing to do, exit.
+                        break;
+                    }
+
+                    if (_doingWork)
+                    {
+                        // Something to do, but DoWork has been rescheduled already, exit.
+                        break;
+                    }
+                    else
+                    {
+                        // Something to do, and not rescheduled yet, reactivate. 
+                        // As we are already running and can do the work.
+                        _doingWork = true;
                     }
                 }
             }
